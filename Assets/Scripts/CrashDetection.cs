a86061cafe637c0710f38ef74013ee71550c88d2
RJLawrence1 <lawren610@morrisville.edu>
2025-10-01 14:20:54 -0400

    Added more player collisions and invoke scripts

---
+++ Assets/Scripts/CrashDetection.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetection : MonoBehaviour
+{
+    [SerializeField] float restartDelay = 1f;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+
+        if (collision.gameObject.layer == layerIndex)
+        {
+            Invoke("ReloadScene", restartDelay);
+        }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] float restartDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Player");
+
+        if (collision.gameObject.layer == layerIndex )
+        {
+            Invoke("ReloadScene", restartDelay);
+        }
+        
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
